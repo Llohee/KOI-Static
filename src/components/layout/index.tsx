32c88f286cda6f8a002b635b4d716db636fead0b
Llohee <nggiang141@gmail.com>
2025-06-27 10:06:14 +0700

    fix build

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "./header";
 import LanguageRedirect from "@/pages/LanguageRedirect";
 import { useLanguageSync } from "@/i18n/routing";
-import { Toaster } from "../ui/sonner";
 
 export default function Layout() {
   useLanguageSync();
